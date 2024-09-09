f1a5b9d59619df16566f137bfcdaf485da20eed8
zmortada <zeinab.mortada@minesparis.psl.eu>
2024-09-09 21:59:18 +0200

    Update main.py

--- P1/P1C5/statement/main.py
+++ P1/P1C5/statement/main.py
@@ -1,1 +1,20 @@
 # Write your code here!
+name="Zeinab"
+age="23"
+height="1.62"
+is_student=True
+# Display the value of all variables
+print(f"Nom: {name}")
+print(f"Age: {age}")
+print(f"Taille: {height}")
+print(f"Est étudiant: {is_student}")
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"height: {height}")
+print(f"is student: {is_student}")
+
+# Display the type of all variables
+print(f"Type name: {type(name)}")
+print(f"Type age: {type(age)}")
+print(f"Type height: {type(height)}")
+print(f"Type is student: {type(is_student)}")
