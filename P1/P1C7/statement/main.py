8a010451a7089158b0806b3ca21dbf172fca53d9
zmortada <zeinab.mortada@minesparis.psl.eu>
2024-09-10 11:22:10 +0200

    Update main.py

--- P1/P1C7/statement/main.py
+++ P1/P1C7/statement/main.py
@@ -1,1 +1,7 @@
 # Write your code here!
+fruits={"apple":"red","banana":"yellow","orange":"orange"}
+fruits["kiwi"]="green"
+banana_color=fruits["banana"]
+fruits["apple"]="green"
+del fruits["ornage"]
+print(fruits.keys())
